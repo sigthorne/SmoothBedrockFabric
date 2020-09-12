ce0545787bd1f52f83b3d79daefe6ec3f441dbc9
sigthorne <sigthorne@gmail.com>
2020-09-12 11:18:56 +0200

    Update gradle for mc1.16.3 and port FlatBedrock mixin

--- src/main/java/me/xa5/smoothbedrock/mixin/MixinFlatBedrock.java
+++ src/main/java/me/xa5/smoothbedrock/mixin/MixinFlatBedrock.java
@@ -4,14 +4,14 @@ import me.xa5.smoothbedrock.SmoothBedrock;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.ChunkRegion;
 import net.minecraft.world.biome.source.BiomeSource;
 import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.gen.ChunkRandom;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
-import net.minecraft.world.gen.chunk.ChunkGeneratorType;
+import net.minecraft.world.gen.chunk.ChunkGeneratorSettings;
 import net.minecraft.world.gen.chunk.StructuresConfig;
-import net.minecraft.world.gen.chunk.SurfaceChunkGenerator;
+import net.minecraft.world.gen.chunk.NoiseChunkGenerator;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -21,60 +21,51 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Iterator;
 import java.util.Random;
+import java.util.function.Supplier;
 
-@Mixin(SurfaceChunkGenerator.class)
+@Mixin(NoiseChunkGenerator.class)
 public abstract class MixinFlatBedrock extends ChunkGenerator {
     @Shadow
     @Final
-    protected ChunkGeneratorType field_24774;
+    protected Supplier<ChunkGeneratorSettings> settings;
+
     @Shadow
     @Final
-    private int field_24779; // worldHeight
-    @Shadow
-    @Final
-    protected ChunkRandom random;
-    ThreadLocal<Identifier> dimId = new ThreadLocal<>();
+    private int worldHeight;
 
     public MixinFlatBedrock(BiomeSource biomeSource_1, StructuresConfig chunkGeneratorConfig_1) {
         super(biomeSource_1, chunkGeneratorConfig_1);
     }
 
-    @Inject(method = "buildSurface", at = @At("HEAD"), cancellable = true)
-    public void test(ChunkRegion region, Chunk chunk, CallbackInfo info) {
-        // TODO why is this deprecated? Maybe there's a better way to get the ID.
-        dimId.set(region.getWorld().getDimensionRegistryKey().getValue());
+    @Inject(method = "buildSurface", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/gen/chunk/NoiseChunkGenerator;buildBedrock(Lnet/minecraft/world/chunk/Chunk;Ljava/util/Random;)V"), cancellable = true)
+    public void buildSurface(ChunkRegion region, Chunk chunk, CallbackInfo info) {
+        Identifier dimId = region.getRegistryManager().get(Registry.DIMENSION_TYPE_KEY).getId(region.getDimension());
+
+        if (SmoothBedrock.getInstance().shouldModifyBedrock(dimId)) {
+            buildFlatBedrock(chunk);
+            info.cancel();
+        }
     }
 
-    @Inject(method = "buildBedrock", at = @At("HEAD"), cancellable = true)
-    private void buildBedrock(Chunk chunk, Random rand, CallbackInfo info) {
-        if (SmoothBedrock.getInstance().shouldModifyBedrock(dimId.get())) {
-            info.cancel();
+    private void buildFlatBedrock(Chunk chunk) {
+        BlockPos.Mutable mutable = new BlockPos.Mutable();
+        int chunkStartX = chunk.getPos().getStartX();
+        int chunkStartZ = chunk.getPos().getStartZ();
+        ChunkGeneratorSettings chunkGeneratorSettings = this.settings.get();
+        int bedrockFloor = chunkGeneratorSettings.getBedrockFloorY();
+        int bedrockRoof = this.worldHeight - 1 - chunkGeneratorSettings.getBedrockCeilingY();
+        boolean generateRoof = bedrockRoof + 4 >= 0 && bedrockRoof < this.worldHeight;
+        boolean generateFloor = bedrockFloor + 4 >= 0 && bedrockFloor < this.worldHeight;
 
-            BlockPos.Mutable mutable = new BlockPos.Mutable();
-            int chunkStartX = chunk.getPos().getStartX();
-            int chunkStartZ = chunk.getPos().getStartZ();
-            int bedrockFloor = this.field_24774.getBedrockFloorY();
-            int bedrockRoof = field_24779 - 1 - this.field_24774.getBedrockCeilingY();
-            boolean generateRoof = bedrockRoof + 4 >= 0 && bedrockRoof < this.field_24779;
-            boolean generateFloor = bedrockFloor + 4 >= 0 && bedrockFloor < this.field_24779;
+        if (generateRoof) {
+            for (BlockPos blockPos : BlockPos.iterate(chunkStartX, 0, chunkStartZ, chunkStartX + 15, 0, chunkStartZ + 15)) {
+                chunk.setBlockState(mutable.set(blockPos.getX(), bedrockRoof, blockPos.getZ()), Blocks.BEDROCK.getDefaultState(), false);
+            }
+        }
 
-            if (generateFloor || generateRoof) {
-                Iterator<BlockPos> chunkBlocks = BlockPos.iterate(chunkStartX, 0, chunkStartZ, chunkStartX + 15, 0, chunkStartZ + 15).iterator();
-                while (true) {
-                    BlockPos blockPos;
-                    do {
-                        if (!chunkBlocks.hasNext()) {
-                            return;
-                        }
-
-                        blockPos = chunkBlocks.next();
-                        if (generateRoof) {
-                            chunk.setBlockState(mutable.set(blockPos.getX(), bedrockRoof, blockPos.getZ()), Blocks.BEDROCK.getDefaultState(), false);
-                        }
-                    } while (!generateFloor);
-
-                    chunk.setBlockState(mutable.set(blockPos.getX(), bedrockFloor, blockPos.getZ()), Blocks.BEDROCK.getDefaultState(), false);
-                }
+        if (generateFloor) {
+            for (BlockPos blockPos : BlockPos.iterate(chunkStartX, 0, chunkStartZ, chunkStartX + 15, 0, chunkStartZ + 15)) {
+                chunk.setBlockState(mutable.set(blockPos.getX(), bedrockFloor, blockPos.getZ()), Blocks.BEDROCK.getDefaultState(), false);
             }
         }
     }
